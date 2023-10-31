1729c0e892121e5f0438b19e7b33aa10a03a119a
= <=>
2023-10-31 19:15:33 +0530

    Adding Expense 2 firebase

--- src/Components/MainContent/Home/Welcome/ExpenseForm/ExpenseForm.js
+++ src/Components/MainContent/Home/Welcome/ExpenseForm/ExpenseForm.js
@@ -1,15 +1,45 @@
-import React, {  useContext, useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { Form, Row, Col, Button, Container } from "react-bootstrap";
 import "./ExpenseForm.css";
 import DataContext from "../../../../../store/data-context";
 
 export default (props) => {
+  const amountRef = useRef();
+  const descriptionRef = useRef();
+  const categoryRef = useRef();
 
-    const amountRef=useRef()
-    const descriptionRef=useRef()
-    const categoryRef=useRef()
+  function addExpense2Firebase(obj) {
+    fetch(
+      "https://expense-tracker-react-77fb5-default-rtdb.firebaseio.com/expenses.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.error) {
+          const data = result;
+          let errorMessage = "Somthing went wrong";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        } else {
+          //alert("Login Successfull");
 
-    const dataContext=useContext(DataContext)
+          console.log(result);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+  const dataContext = useContext(DataContext);
   const categories = [
     "Food",
     "Petrol",
@@ -22,24 +52,23 @@ export default (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
 
-    dataContext.setExpenseList(prevState=>{
+    dataContext.setExpenseList((prevState) => {
+      const newObj = [...prevState];
+      const obj = {
+        amount: amountRef.current.value,
+        description: descriptionRef.current.value,
+        category: categoryRef.current.value,
+      };
+      newObj.push(obj);
+      
+      addExpense2Firebase(obj)
+      amountRef.current.value = "";
+      descriptionRef.current.value = "";
+      categoryRef.current.value = "Food";
+      return newObj;
+    });
 
-        const newObj=[...prevState]
-        const obj={
-            amount:amountRef.current.value,
-            description:descriptionRef.current.value,
-            category:categoryRef.current.value
-        }
-        newObj.push(obj)
-
-        return newObj;
-    })
-
-    amountRef.current.value=''
-    descriptionRef.current.value=''
-    categoryRef.current.value='Food'
     
   };
 
@@ -83,7 +112,12 @@ export default (props) => {
           </Col>
 
           <Col className="col-9">
-            <Form.Control as="select" ref={categoryRef} name="category" className="input-width">
+            <Form.Control
+              as="select"
+              ref={categoryRef}
+              name="category"
+              className="input-width"
+            >
               {categories.map((category, index) => (
                 <option key={index}>{category}</option>
               ))}
--- src/Components/MainContent/Home/Welcome/ExpenseList/Expenses.js
+++ src/Components/MainContent/Home/Welcome/ExpenseList/Expenses.js
@@ -1,10 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpenseList from "./ExpenseList";
 import DataContext from "../../../../../store/data-context";
 import { Container, Table } from "react-bootstrap";
 
 export default (props) => {
   const dataContext = useContext(DataContext);
+  useEffect(() => {
+    fetch(
+      "https://expense-tracker-react-77fb5-default-rtdb.firebaseio.com/expenses.json"
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.error) {
+          const data = result;
+          let errorMessage = "Somthing went wrong";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        } else {
+          //alert("Login Successfull");
+
+         const list=[]
+          for(const x in result){
+            list.push(result[x])
+          }
+          dataContext.setExpenseList(list)
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, []);
 
   const expenseList = dataContext.expenseList.map((item) => {
     return <ExpenseList key={Math.random().toString()} item={item} />;
@@ -13,7 +40,7 @@ export default (props) => {
   return (
     <>
       <Container className="my-5">
-        <Table striped  hover>
+        <Table striped hover>
           <thead>
             <tr>
               <th>Amount ($)</th>
@@ -21,9 +48,7 @@ export default (props) => {
               <th>Category</th>
             </tr>
           </thead>
-          <tbody>
-            {expenseList}
-          </tbody>
+          <tbody>{expenseList}</tbody>
         </Table>
       </Container>
     </>
